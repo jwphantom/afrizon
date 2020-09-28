317a6e38044f34708766a8bcd9bd200d76544271
jwphantom <44881349+jwphantom@users.noreply.github.com>
2020-09-28 17:19:39 +0100

    fix bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'afrizone';
+
+  constructor(
+    private title: Title
+  ) { }
+
+  ngOnInit(){   
+    
+    this.title.setTitle("Afrizon - Home");
+
+    this.loadScript('../assets/js/jquery.js');
+    this.loadScript('../assets/js/plugins.js');
+    this.loadScript('../assets/js/functions.js');
+
+  }
+
+  public loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
 
   onActivate(event) {
     let scrollToTop = window.setInterval(() => {
